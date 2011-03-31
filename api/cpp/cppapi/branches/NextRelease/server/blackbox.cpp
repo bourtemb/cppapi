4ad5d2f9198853024b7c2b4d9579dd374689fce1
taurel <taurel@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-03-31 12:48:03 +0000

    No change, only to test Jenkins

--- api/cpp/cppapi/branches/NextRelease/server/blackbox.cpp
+++ api/cpp/cppapi/branches/NextRelease/server/blackbox.cpp
@@ -35,7 +35,6 @@ static const char *RcsId = "$Id$\n$Name$";
 // 
 // You should have received a copy of the GNU Lesser General Public License
 // along with Tango.  If not, see <http://www.gnu.org/licenses/>.
-
 //
 // $Revision$
 //
