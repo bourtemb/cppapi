3864487550785eb16f81e48263260adf9b09da42
taurel <taurel@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-11-10 08:25:48 +0000

    Fix problem when ZMQ event is an error

--- api/cpp/cppapi/branches/NextRelease/server/zmqeventsupplier.cpp
+++ api/cpp/cppapi/branches/NextRelease/server/zmqeventsupplier.cpp
@@ -178,7 +178,7 @@ ZmqEventSupplier::ZmqEventSupplier(Util *tg):EventSupplier(tg),zmq_context(1),ev
     event_call_nok_mess.rebuild(event_call_nok_cdr.bufSize());
     memcpy(event_call_nok_mess.data(),event_call_nok_cdr.bufPtr(),event_call_nok_cdr.bufSize());
 
-    event_call_nok_mess_2.copy(&event_call_nok_mess_2);
+    event_call_nok_mess_2.copy(&event_call_nok_mess);
 
 //
 // Start to init the event name used for the DS heartbeat event
@@ -533,7 +533,9 @@ void ZmqEventSupplier::push_heartbeat_event()
         zmq::message_t name_mess(heartbeat_event_name.size());
         memcpy(name_mess.data(),(void *)heartbeat_event_name.data(),heartbeat_event_name.size());
 
-		bool fail = false;
+		bool endian_mess_sent = false;
+		bool call_mess_sent = false;
+
 		try
 		{
 //
@@ -572,7 +574,9 @@ void ZmqEventSupplier::push_heartbeat_event()
 
             heartbeat_pub_sock->send(name_mess,ZMQ_SNDMORE);
 			heartbeat_pub_sock->send(endian_mess,ZMQ_SNDMORE);
+			endian_mess_sent = true;
 			heartbeat_pub_sock->send(heartbeat_call_mess,0);
+			call_mess_sent = true;
 
 //
 // For reference counting on zmq messages which do not have a local scope
@@ -584,22 +588,23 @@ void ZmqEventSupplier::push_heartbeat_event()
 		catch(...)
 		{
 			cout3 << "ZmqEventSupplier::push_heartbeat_event() failed !\n";
-			fail = true;
+			if (endian_mess_sent == true)
+                endian_mess.copy(&endian_mess_2);
+            if (call_mess_sent == true)
+                heartbeat_call_mess.copy(&heartbeat_call_mess_2);
+
+            TangoSys_OMemStream o;
+            o << "Can't push ZMQ heartbeat event for event ";
+            o << heartbeat_event_name;
+            if (zmq_errno() != 0)
+                o << "\nZmq error: " << zmq_strerror(zmq_errno()) << ends;
+            else
+                o << ends;
+
+            Except::throw_exception((const char *)"DServer_Events",
+                                    o.str(),
+                                   (const char *)"ZmqEventSupplier::push_heartbeat_event");
 		}
-
-//
-// If it was not possible to communicate with notifd,
-// try a reconnection
-//
-
-//		if (fail == true)
-//		{
-//			try
-//			{
-//				reconnect_notifd();
-//			}
-//			catch (...) {}
-//		}
 	}
 }
 
@@ -771,7 +776,9 @@ void ZmqEventSupplier::push_event(DeviceImpl *device_impl,string event_type,
 // Send the data
 //
 
-    bool fail = false;
+    bool endian_mess_sent = false;
+    bool call_mess_sent = false;
+
     try
     {
 
@@ -833,10 +840,12 @@ void ZmqEventSupplier::push_event(DeviceImpl *device_impl,string event_type,
 
         pub->send(name_mess,ZMQ_SNDMORE);
         pub->send(endian_mess,ZMQ_SNDMORE);
+        endian_mess_sent = true;
         if (except == NULL)
             pub->send(event_call_ok_mess,ZMQ_SNDMORE);
         else
             pub->send(event_call_nok_mess,ZMQ_SNDMORE);
+        call_mess_sent = true;
         pub->send(data_mess,0);
 
         if (large_data == false)
@@ -851,13 +860,34 @@ void ZmqEventSupplier::push_event(DeviceImpl *device_impl,string event_type,
             event_call_ok_mess.copy(&event_call_ok_mess_2);
         else
             event_call_nok_mess.copy(&event_call_nok_mess_2);
+
     }
     catch(...)
     {
-        cout3 << "ZmqEventSupplier::push_event() failed !\n";
+        cout3 << "ZmqEventSupplier::push_event() failed !!!!!!!!!!!\n";
+        if (endian_mess_sent == true)
+            endian_mess.copy(&endian_mess_2);
+        if (call_mess_sent == true)
+        {
+            if (except == NULL)
+                event_call_ok_mess.copy(&event_call_ok_mess_2);
+            else
+                event_call_nok_mess.copy(&event_call_nok_mess_2);
+        }
         if (large_message_created == false)
             push_mutex.unlock();
-        fail = true;
+
+        TangoSys_OMemStream o;
+        o << "Can't push ZMQ event for event ";
+        o << event_name;
+        if (zmq_errno() != 0)
+            o << "\nZmq error: " << zmq_strerror(zmq_errno()) << ends;
+        else
+            o << ends;
+
+        Except::throw_exception((const char *)"DServer_Events",
+                                    o.str(),
+                                   (const char *)"ZmqEventSupplier::push_event");
     }
 }
 
