ba9712e9d62ebfe95ea992c384bd940df598d39f
taurel <taurel@users.noreply.github.com>
2013-03-12 09:12:16 +0000

    - Fix bug in cache management which in some cases could prevent the TAC to start.

--- client/dbapi_cache.cpp
+++ client/dbapi_cache.cpp
@@ -748,11 +748,19 @@ const DevVarStringArray *DbServerCache::get_dev_att_property(DevVarStringArray *
 	}
 	else
 	{
-		TangoSys_OMemStream o;
-		o << "Device " << (*in_param)[0] << " not found in DB cache" << ends;
+		if (TG_strncasecmp("dserver/",(*in_param)[0],8) != 0)
+		{
+			TangoSys_OMemStream o;
+			o << "Device " << (*in_param)[0] << " not found in DB cache" << ends;
 
-		Tango::Except::throw_exception((const char *)"DB_DeviceNotFoundInCache",o.str(),
-									   (const char *)"DbServerCache::get_dev_att_property");
+			Tango::Except::throw_exception((const char *)"DB_DeviceNotFoundInCache",o.str(),
+											(const char *)"DbServerCache::get_dev_att_property");
+		}
+		else
+		{
+			::sprintf(n_att_str,"%d",found_att);
+			ret_obj_att_prop[1] = CORBA::string_dup(n_att_str);
+		}
 	}
 
 	cout4 << "DbCache --> Returned data for a get_dev_att_property for device " << (*in_param)[0] << endl;
